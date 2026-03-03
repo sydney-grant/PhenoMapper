2f32995cbaacacb6db918513358941cd1c30ba1b
sydney-grant <sgrant@coh.org>
2026-03-02 17:00:36 -0800

    fix warnings

--- R/PhenoMapper.R
+++ R/PhenoMapper.R
@@ -1,7 +1,16 @@
 #' PhenoMapper
 #' @name PhenoMapper
 #' @aliases PhenoMapper
-#'
+#' @usage PhenoMapper
 #' @title PhenoMapper: single sample pathway scores for genomic data
+#' @keywords packages
 #'
+#' @format a software package with four functions and 5 data files
+#'
+#' @description
+#' A software package for downloading pretrained models to calculate
+#' single sample pathway scores from genomic datasets
+#'
+#' @return a software packages for downloading pretrained models to calculate
+#' single sample pathway scores from genomic datasets
 NULL
